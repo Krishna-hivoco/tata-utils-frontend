8884b5bb0029c35b231c09a50a48be53a3fe20c5
Krishna-hivoco <krishna@hivoco.com>
2024-12-13 14:27:22 +0530

    upload new photo button

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [responseMessage, setResponseMessage] = useState(null);
-  const [base64, setBase64] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   const handleDrop = (event) => {
     event.preventDefault();
@@ -21,13 +21,8 @@ export default function App() {
         return;
       }
 
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const base64String = e.target.result;
-        setPreview(base64String);
-        setBase64(base64String);
-      };
-      reader.readAsDataURL(file); // Read file as Base64
+      setPreview(URL.createObjectURL(file));
+      setSelectedFile(file);
     } else {
       setError("Please drop a valid image file.");
     }
@@ -43,19 +38,13 @@ export default function App() {
         return;
       }
 
-      const blob = new Blob([file], { type: file.type });
-      console.log("Blob object:", blob);
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const base64String = e.target.result;
-        setPreview(base64String);
-        setBase64(base64String);
-      };
-      reader.readAsDataURL(file);
+      setPreview(URL.createObjectURL(file));
+      setSelectedFile(file);
     } else {
       setError("Please select a valid image file.");
     }
   };
+
   const triggerFileUpload = () => {
     document.getElementById("fileInput").click();
   };
@@ -65,14 +54,26 @@ export default function App() {
   };
 
   const handleClick = async () => {
+    if (!selectedFile) {
+      setError("No file selected. Please upload a file first.");
+      return;
+    }
+
     setLoading(true);
     setResponseMessage(null);
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
 
     try {
       const response = await axios.post(
         "https://sampann-open.thefirstimpression.ai/process/image",
+        // "http://192.168.1.3:8509/process/image",
+        formData,
         {
-          image: base64,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
         }
       );
       setResponseMessage(response.data.message);
@@ -83,6 +84,7 @@ export default function App() {
       setLoading(false);
     }
   };
+
   return (
     <div>
       <header className="h-20 shadow-sm bg-white w-full flex justify-center items-center">
@@ -92,19 +94,17 @@ export default function App() {
         style={{ minHeight: "calc(100vh - 90px)" }}
         className="mt-[10px] bg-white shadow-sm md:p-20 p-10"
       >
-        <div className=" max-w-[400px] p-5 text-center mx-auto bg-[#F8F9FE] border-4 rounded-2xl  border-dotted w-full h-full flex flex-col justify-center items-center cursor-pointer">
-          <div onDrop={handleDrop} onDragOver={handleDragOver}>
-            <img src="/upload.png" alt="" srcSet="" />
-            <strong className="font-bold text-lg uppercase mt-3">
-              UPLOAD IMAGE HERE
-            </strong>
-            <br />
-            {error && (
-              <small className=" text-xs text-red-600 ">
-                File size must be less than 10MB.
-              </small>
-            )}
-          </div>
+        <div
+          className=" max-w-[400px] p-5 text-center mx-auto bg-[#F8F9FE] border-4 rounded-2xl  border-dotted w-full h-full flex flex-col justify-center items-center cursor-pointer"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
+          <img src="/upload.png" alt="" srcSet="" />
+          <strong className="font-bold text-lg uppercase mt-3">
+            UPLOAD IMAGE HERE
+          </strong>
+          <br />
+          {error && <small className=" text-xs text-red-600 ">{error}</small>}
           {preview && (
             <img
               src={preview}
@@ -115,17 +115,17 @@ export default function App() {
           {preview ? (
             <>
               <button
-                onClick={() => handleClick()}
+                onClick={handleClick}
                 className={`${
                   loading && "hidden"
                 } font-semibold text-sm uppercase px-9 tracking-wider py-5 bg-black rounded-xl text-white mt-14`}
               >
-                Send File
+                {loading ? "Uploading..." : "Send File"}
               </button>
               <span
                 onClick={() => {
                   setPreview(null);
-                  setBase64(null);
+                  setSelectedFile(null);
                 }}
                 className="cursor-pointer underline text-sm font-medium mt-4"
               >
@@ -159,3 +159,166 @@ export default function App() {
     </div>
   );
 }
+
+// import axios from "axios";
+// import { useState } from "react";
+
+// export default function App() {
+//   const [preview, setPreview] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [responseMessage, setResponseMessage] = useState(null);
+//   const [base64, setBase64] = useState(null);
+
+//   const handleDrop = (event) => {
+//     event.preventDefault();
+//     setError(null); // Clear previous errors
+//     const files = event.dataTransfer.files;
+
+//     if (files.length > 0 && files[0].type.startsWith("image/")) {
+//       const file = files[0];
+
+//       if (file.size > 10 * 1024 * 1024) {
+//         setError("File size must be less than 10MB.");
+//         return;
+//       }
+
+//       const reader = new FileReader();
+//       reader.onload = (e) => {
+//         const base64String = e.target.result;
+//         setPreview(base64String);
+//         setBase64(base64String);
+//       };
+//       reader.readAsDataURL(file); // Read file as Base64
+//     } else {
+//       setError("Please drop a valid image file.");
+//     }
+//   };
+
+//   const handleFileChange = (event) => {
+//     setError(null); // Clear previous errors
+//     const file = event.target.files[0];
+
+//     if (file && file.type.startsWith("image/")) {
+//       if (file.size > 10 * 1024 * 1024) {
+//         setError("File size must be less than 10MB.");
+//         return;
+//       }
+
+//       const blob = new Blob([file], { type: file.type });
+//       console.log("Blob object:", blob);
+//       const reader = new FileReader();
+//       reader.onload = (e) => {
+//         const base64String = e.target.result;
+//         setPreview(base64String);
+//         setBase64(base64String);
+//       };
+//       reader.readAsDataURL(file);
+//     } else {
+//       setError("Please select a valid image file.");
+//     }
+//   };
+//   const triggerFileUpload = () => {
+//     document.getElementById("fileInput").click();
+//   };
+
+//   const handleDragOver = (event) => {
+//     event.preventDefault();
+//   };
+
+//   const handleClick = async () => {
+//     setLoading(true);
+//     setResponseMessage(null);
+
+//     try {
+//       const response = await axios.post(
+//         // "http://192.168.1.3:8509/process/image",
+//         "https://sampann-open.thefirstimpression.ai/process/image",
+//         {
+//           image: base64,
+//         }
+//       );
+//       setResponseMessage(response.data.message);
+//     } catch (error) {
+//       console.error("API Error:", error);
+//       setResponseMessage("Failed to fetch data. Please try again.");
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+//   return (
+//     <div>
+//       <header className="h-20 shadow-sm bg-white w-full flex justify-center items-center">
+//         <img src="/logo.png" alt="logo" srcSet="" />
+//       </header>
+//       <section
+//         style={{ minHeight: "calc(100vh - 90px)" }}
+//         className="mt-[10px] bg-white shadow-sm md:p-20 p-10"
+//       >
+//         <div className=" max-w-[400px] p-5 text-center mx-auto bg-[#F8F9FE] border-4 rounded-2xl  border-dotted w-full h-full flex flex-col justify-center items-center cursor-pointer">
+//           <div onDrop={handleDrop} onDragOver={handleDragOver}>
+//             <img src="/upload.png" alt="" srcSet="" />
+//             <strong className="font-bold text-lg uppercase mt-3">
+//               UPLOAD IMAGE HERE
+//             </strong>
+//             <br />
+//             {error && (
+//               <small className=" text-xs text-red-600 ">
+//                 File size must be less than 10MB.
+//               </small>
+//             )}
+//           </div>
+//           {preview && (
+//             <img
+//               src={preview}
+//               alt="Preview"
+//               className="mt-5 h-52 object-contain  border rounded-lg shadow-md"
+//             />
+//           )}
+//           {preview ? (
+//             <>
+//               <button
+//                 onClick={() => handleClick()}
+//                 className={`${
+//                   loading && "hidden"
+//                 } font-semibold text-sm uppercase px-9 tracking-wider py-5 bg-black rounded-xl text-white mt-14`}
+//               >
+//                 Send File
+//               </button>
+//               <span
+//                 onClick={() => {
+//                   setPreview(null);
+//                   setBase64(null);
+//                 }}
+//                 className="cursor-pointer underline text-sm font-medium mt-4"
+//               >
+//                 Upload new file
+//               </span>
+//             </>
+//           ) : (
+//             <button
+//               onClick={triggerFileUpload}
+//               className="font-semibold text-sm uppercase px-9 tracking-wider py-5 bg-black rounded-xl text-white mt-14"
+//             >
+//               BROWSE FILES
+//             </button>
+//           )}
+//           <input
+//             id="fileInput"
+//             type="file"
+//             accept="image/*"
+//             onChange={handleFileChange}
+//             className="hidden"
+//           />
+//         </div>
+//       </section>
+//       {responseMessage && (
+//         <section className="bg-white">
+//           <div className="w-[400px] md:w-[700px] p-10 m-auto bg-[#F8F9FE] rounded-xl tracking-widest">
+//             <p className="font-normal text-xl text-black">{responseMessage}</p>
+//           </div>
+//         </section>
+//       )}
+//     </div>
+//   );
+// }
